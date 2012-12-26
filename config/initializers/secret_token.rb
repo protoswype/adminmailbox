f0b103af10892f4ad5ab12f1b8f49d1b312cdb04
Michael Koriath <your_email@youremail.com>
2012-12-26 23:39:13 +0100

    Initial Commit | Creating Rails-App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adminmailbox::Application.config.secret_token = '0131d72afca2388413bc4ef55f66b8817d1a48a39a4766ec432a2b5b48fc21766d2c5573a0a1543bf588a41781b503a75b9468b0be14c3acd533c4fe78c94518'
